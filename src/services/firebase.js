44251a65cdc54bc1382cdf62687b7bb19d9ff41d
Zoi Papamichail <109663228+zopapami@users.noreply.github.com>
2023-01-27 18:53:51 +0200

    firebase - env file

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -4,26 +4,14 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBtZfFWR73SC_mhSO-C5dxzjl6VWvnFyQA",
-  authDomain: "react-crud-ed285.firebaseapp.com",
-  databaseURL: "https://react-crud-ed285-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "react-crud-ed285",
-  storageBucket: "react-crud-ed285.appspot.com",
-  messagingSenderId: "692975450992",
-  appId: "1:692975450992:web:19298fa4a053c7e929adb5"
+  apiKey: process.env.REACT_APP_APP_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
-
-/* 
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_APP_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID
-};
-*/
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
